b4d1e47f918fc51c9379c156608ed797bd959b84
irecaldema14dw@ikzubirimanteo.com <zubiri@Ubuntu-12-04.(none)>
2014-10-15 14:10:29 +0200

    comentando cada linea

--- src/Control7.java
+++ src/Control7.java
@@ -1,27 +1,27 @@
 /*
  * programa que lee una nota y escribe la calificación correspondiente
  */
-import java.util.*;
-public class Control7 {
-    public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      double nota;
-      System.out.println("Introduzca una nota entre 0 y 10: ");
-      nota = sc.nextDouble();
-      System.out.println("La calificación del alumno es ");
-      if(nota < 0 || nota > 10)
-         System.out.println("Nota no válida");
-      else if(nota==10)
-           System.out.println("Matrícula de Honor");
-      else if (nota >= 9)
-           System.out.println("Sobresaliente");
-      else if (nota >= 7)
-           System.out.println("Notable");
-      else if (nota >= 6)
-           System.out.println("Bien");
-      else if (nota >= 5)
-           System.out.println("Suficiente");
-      else
-           System.out.println("Suspenso");
+import java.util.*;//importa librerias
+public class Control7 {//crea clase
+    public static void main(String[] args) {//inicia ejecucion
+      Scanner sc = new Scanner(System.in);//crea objeto scanner
+      double nota;//declara variable
+      System.out.println("Introduzca una nota entre 0 y 10: ");//muestra mensaje
+      nota = sc.nextDouble();//escanea entrada
+      System.out.println("La calificación del alumno es ");//muestra mensaje
+      if(nota < 0 || nota > 10)//si la variable es menor a 0 o es mayor a 10
+         System.out.println("Nota no válida");//muestra mensaje
+      else if(nota==10)//si no se cumple la condicion pero la variable es igual a 10
+           System.out.println("Matrícula de Honor");//muestra mensaje
+      else if (nota >= 9)//si no se cumple la condicion pero la variable es mayor o igual a 9
+           System.out.println("Sobresaliente");//muestra mensaje
+      else if (nota >= 7)//si no se cumple la condicion pero la variable es mayor o igual a 7
+           System.out.println("Notable");//muestra mensaje
+      else if (nota >= 6)//si no se cumple la condicion pero la variable es mayor o igual a 6
+           System.out.println("Bien");//muestra mensaje
+      else if (nota >= 5)//si no se cumple la condicion pero la variable es mayor o igual a 5
+           System.out.println("Suficiente");//muestra mensaje
+      else//si no se cumplen las condiciones
+           System.out.println("Suspenso");//muestra mensaje
     }
 }
--- src/Control8.java
+++ src/Control8.java
@@ -1,40 +1,40 @@
 /*
  * Programa que pide un número de mes y muestra el nombre correspondiente
  */
-import java.util.*;
-public class Control8 {
-    public static void main(String[] args) {
-        int mes;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introduzca un numero de mes: ");
-        mes = sc.nextInt();
-        switch (mes)
+import java.util.*;//importa librerias
+public class Control8 {//crea clase
+    public static void main(String[] args) {//inicia ejecucion
+        int mes;//declara variable
+        Scanner sc = new Scanner(System.in);//crea objeto scanner
+        System.out.print("Introduzca un numero de mes: ");//muestra mensaje
+        mes = sc.nextInt();//escanea entrada
+        switch (mes)//si la variable
         {
-                case 1: System.out.println("ENERO");
-                           break;
-                case 2: System.out.println("FEBRERO");
-                           break;
-                case 3: System.out.println("MARZO");
-                           break;
-                case 4: System.out.println("ABRIL");
-                           break;
-                case 5: System.out.println("MAYO");
-                           break;
-                case 6: System.out.println("JUNIO");
-                           break;
-                case 7: System.out.println("JULIO");
-                           break;
-                case 8: System.out.println("AGOSTO");
-                           break;
-                case 9: System.out.println("SEPTIEMBRE");
-                           break;
-                case 10: System.out.println("OCTUBRE");
-                             break;
-                case 11: System.out.println("NOVIEMBRE");
-                             break;
-                case 12: System.out.println("DICIEMBRE");
-                             break;
-                default : System.out.println("Mes no válido");                       
+                case 1: System.out.println("ENERO");//es 1 muestra mensaje
+                           break;//sale
+                case 2: System.out.println("FEBRERO");//es 2 muestra mensaje
+                           break;//sale
+                case 3: System.out.println("MARZO");//es 3 muestra mensaje
+                           break;//sale
+                case 4: System.out.println("ABRIL");//es 4 muestra mensaje
+                           break;//sale
+                case 5: System.out.println("MAYO");//es 5 muestra mensaje
+                           break;//sale
+                case 6: System.out.println("JUNIO");//es 6 muestra mensaje
+                           break;//sale
+                case 7: System.out.println("JULIO");//es 7 muestra mensaje
+                           break;//sale
+                case 8: System.out.println("AGOSTO");//es 8 muestra mensaje
+                           break;//sale
+                case 9: System.out.println("SEPTIEMBRE");//es 9 muestra mensaje
+                           break;//sale
+                case 10: System.out.println("OCTUBRE");//es 10 muestra mensaje
+                             break;//sale
+                case 11: System.out.println("NOVIEMBRE");//es 11 muestra mensaje
+                             break;//sale
+                case 12: System.out.println("DICIEMBRE");//es 12 muestra mensaje
+                             break;//sale
+                default : System.out.println("Mes no válido");//no es ninguno de los otros casos muestra mensaje (por defecto)                  
         }
     }
 }
