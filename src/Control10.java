d084a103d02815fb3d5324e7d0a315c77e0ace71
irecaldema14dw@ikzubirimanteo.com <zubiri@Ubuntu-12-04.(none)>
2014-10-16 08:28:57 +0200

    comentando cada linea

--- src/Control10.java
+++ src/Control10.java
@@ -1,13 +1,13 @@
  /*
   * programa que pide un número por teclado y calcula si es par o impar
   */
-import java.util.*;
-public class Control10 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num;      
-        System.out.println("Introduzca numero: ");
-        num = sc.nextInt();
-        System.out.println((num%2)==0 ? "PAR" : "IMPAR");
-    }
-}
+import java.util.*;//importa librerias
+public class Control10 {//crea clase	
+    public static void main(String[] args) {//inicia ejecucion
+        Scanner sc = new Scanner(System.in);//crea objeto scanner
+        int num;      //declara variable
+        System.out.println("Introduzca numero: ");//muestra mensaje
+        num = sc.nextInt();//escanea entrada
+        System.out.println((num%2)==0 ? "PAR" : "IMPAR");//condicion expresion1 ? Expresion2 : Expresion3, si la condicion es verdadera se ejecuta la espresion1 si es falsa la expresion2
+    }//se cierra la ejecucion
+}//se cierra la clase
