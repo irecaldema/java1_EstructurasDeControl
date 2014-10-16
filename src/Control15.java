cd88ad8893b2f7a7856470519f8891c4f0ff5a42
irecaldema14dw@ikzubirimanteo.com <zubiri@Ubuntu-12-04.(none)>
2014-10-16 09:23:57 +0200

    comentando cada linea

--- src/Control15.java
+++ src/Control15.java
@@ -5,13 +5,21 @@
 import java.util.*;
 //crea clase
 public class Control15 {
+    //inicia eejcucion
     public static void main(String[] args) {
+	//declara variable
         int n;
+	//crea objeto escaner
         Scanner sc = new Scanner( System.in );
+	//bucle do while
         do {
+	    //muestra mensaje
             System.out.print("Escribe un número entre 1 y 10: ");
+	    //escanea numero de entrada
             n = sc.nextInt();
+	//condicion bucle, repeticion cuando la variable sea menor a 1 o mayor a 10
         }while (n<1 || n >10);
+	//muestra mensaje
         System.out.println("Ha introducido: " + n);
     }
 }
