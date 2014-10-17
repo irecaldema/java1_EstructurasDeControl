1fc30c5f2243fec507e7f0c8fe5e62151d148439
irecaldema14dw@ikzubirimanteo.com <zubiri@Ubuntu-12-04.(none)>
2014-10-17 10:15:59 +0200

    comentando cada linea

--- src/Control20.java
+++ src/Control20.java
@@ -6,23 +6,37 @@
 import java.util.*;
 //crea clase
 public class Control20 {
+    //inicia ejecucion
     public static void main(String[] args) {
+	//crea objeto escaner
         Scanner sc = new Scanner(System.in);
+	//declara variables
         int filas, columnas;
         //leer número de filas hasta que sea un número > 0
+	//bucle do while
         do{
+	   //muestra mensaje
            System.out.print("Introduce número de filas: ");
+	   //escanea entrada de numero
            filas = sc.nextInt();
+	//condicion a repetir (cuando filas sea menor a 1)
         }while(filas<1);
         //leer número de columnas hasta que sea un número > 0
+	//bucle do while
         do{
+	   //muestra mensaje
            System.out.print("Introduce número de columnas: ");
+	   //escanea entrada de numero
            columnas = sc.nextInt();
+	//condicion a repetir (cuando columnas sea menor a 1)
         }while(columnas<1);
+	//bucle for (declaracion e inicializacion; condicion a repetir (cuando i sea menor o igual a filas);cambio por cada repeticion)
         for(int i = 1; i<=filas; i++){    //filas
             for(int j = 1; j<=columnas; j++){   //columnas
+		 //muestra mensaje
                  System.out.print(" * ");
             }
+	    //muestra mensaje "vacio" con salto de linea
             System.out.println();
         }
        
--- src/Control21.java
+++ src/Control21.java
@@ -9,12 +9,16 @@ public class Control21 {
 	//declara e inicializa variables
         final int XMAX = 10;
         int x, n;
-        //mostrar la cabecera de la tabla
+        //mostrar la cabecera de la tabla con formato 10 digitos s--> to strring n-->salto de linea
         System.out.printf("%10s%10s%10s%10s%n", "x","x^2","x^3","x^4");
+	//bucle for (inicializacion;condicion a repetir (cuando x sea menor o igual a 4);cambio en variable por repeticion)
         for (x = 1; x <= XMAX; x++){   //filas
+	     ////bucle for (inicializacion;condicion a repetir (cuando n sea menor o igual a 4);cambio en variable por repeticion)
              for (n = 1; n <= 4; n++){   //columnas
+		  //muestra mensaje con formato 10 digitos . 0 digitos en formato float, clase Math metodo pow de power pow(x,n)--> x elevado a n
                   System.out.printf("%10.0f", Math.pow(x,n));
              }
+	     //muestra salto de linea
              System.out.println();
         }
     }
